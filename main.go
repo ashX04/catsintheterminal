b915a6cbf95639625ad6399a3b7bd239ae1624ad
ashX04 <53126404+ashX04@users.noreply.github.com>
2024-09-20 12:06:12 +0530

    simple terminal ascii cats

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Are the cats here yet?")
+	fmt.Println("here they are")
+	fmt.Println("  ^~^  ,")
+	fmt.Println(" ('Y') )")
+	fmt.Println(" /   \\/ ")
+	fmt.Println("(\\|||/)")
 }
